f517838eaa83dbfba4a80112b2c8afb35abd3f3d
David Gundry <d.gundry@yorksj.ac.uk>
2024-12-09 11:01:34 +0000

    Added example test

---
+++ src/test/java/com/example/MyGameTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyGameTest {
+    @Test
+    public void onePlusOneIsTwo() {
+        assertEquals(1+1, 2);
+    }
+}
